d411f82970d9d0bd5bdfecfcbae9015ba22f4ef4
linnchong <ychong@uni-bonn.de>
2023-12-29 19:15:30 +0100

    completed fn lookCW

--- vis_findcontours.cpp
+++ vis_findcontours.cpp
@@ -6,9 +6,66 @@
 
 using namespace std;
 
+// TODO this is only for D8 maybe do one which works for D4 too?
+std::tuple<int, int> lookCW(
+    cv::Mat img, 
+    int pivot_r, 
+    int pivot_c, 
+    int start_r, 
+    int start_c
+    ) 
+{
+  int found_r =-1, found_c = -1;
+  bool is_found = false;
+
+  cout << "r: " << start_r << "; c: " << start_c << endl; 
+  cv::Mat pivot_D8 = img(cv::Rect(pivot_c-1, pivot_r-1, 3, 3));  // TODO safe indexing
+  cout << pivot_D8 << endl;
+
+  // TODO double is really not necessary for this
+  double CW_i_double[3][3] = {
+    { 0, 0, 1}, 
+    {-1, 0, 1}, 
+    {-1, 0, 0}};
+  double CW_j_double[3][3] = {
+    {1,  1,  0}, 
+    {0,  0,  0}, 
+    {0, -1, -1}};
+
+  int d_i, d_j;
+  for(int tick = 0; tick < 8; ++tick)
+  {
+    d_i = start_r - pivot_r + 1; 
+    d_j = start_c - pivot_c + 1;
+
+    start_r = CW_i_double[d_i][d_j] + start_r;
+    start_c = CW_j_double[d_i][d_j] + start_c;
+
+    if(img.at<int>(start_r, start_c) != 0)
+    {
+      found_r = start_r;
+      found_c = start_c;
+      is_found = true;
+      break;
+    }
+
+    // cout << "r: " << start_r << "; c: " << start_c << endl; 
+  }
+
+  if(!is_found)
+  {
+      found_r = -1;
+      found_c = -1;
+  }
+  return make_tuple(found_r, found_c);
+}
+
 
 cv::Mat findcontours(cv::Mat bin_input)
 {
+  bin_input.convertTo(bin_input, CV_32S);  // needs to be able to hold negative integers
+  // TODO check the max number of components in the image, choose datatype based on range required.
+
   int num_rows = bin_input.rows;
   int num_cols = bin_input.cols;
   // cout << "bin img rows: " << num_rows << "; cols: " << num_cols << endl;  
@@ -16,7 +73,7 @@ cv::Mat findcontours(cv::Mat bin_input)
   // raster scan
   int f_ij, i1, j1, i2, j2, i3, j3, i4, j4;
   int NBD, LNBD;
-  bool is_detect_border = False; // Whether or not to do border following (step 3)
+  bool is_detect_border = false; // Whether or not to do border following (step 3)
   NBD = 1;
 
   for (int i = 0; i < num_rows; i++)
@@ -36,7 +93,7 @@ cv::Mat findcontours(cv::Mat bin_input)
           NBD++;
           i2 = i;
           j2 = j-1;
-          is_detect_border = True;
+          is_detect_border = true;
         }
         else if( (f_ij >= 1) && (bin_input.at<uchar>(i,j+1) == 0 ))  // 1(b) hole border
         {
@@ -48,7 +105,7 @@ cv::Mat findcontours(cv::Mat bin_input)
           {
             LNBD = f_ij;
           }
-          is_detect_border = True;
+          is_detect_border = true;
         }
 
         // 2 
@@ -57,7 +114,15 @@ cv::Mat findcontours(cv::Mat bin_input)
 
         if(is_detect_border)
         {
-          is_detect_border = False;
+          is_detect_border = false;
+
+          // 3 
+          // 3.1
+          // look around clockwise
+          tie(i1, j1) = lookCW(bin_input, i, j, i2, j2);
+          cout << i1 <<", " << j1 << endl;
+
+
 
         }
 
